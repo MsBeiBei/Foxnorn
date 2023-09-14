1708a9cdbebf577297cf5573c7d93b71c82e1b39
BeiBei <Ms_BeiBei@yeah.net>
2023-09-14 22:58:43 +0800

    初始化

--- rush-plugins/rush-sortpack/src/helpers/getRushConfig.ts
+++ rush-plugins/rush-sortpack/src/helpers/getRushConfig.ts
@@ -3,18 +3,16 @@ import { CWD } from "../constants.js";
 
 export const localConfig = new Map<string, RushLib.RushConfiguration>();
 
-
 export function getRushConfig(cwd: string = CWD): RushLib.RushConfiguration {
+
   if (localConfig.has(cwd)) {
     return localConfig.get(cwd) as RushLib.RushConfiguration;
   }
 
-  console.log(cwd)
-
   const { RushConfiguration } = RushLib;
 
   if (!RushConfiguration) {
-    throw new Error('load RushConfiguration from rush-sdk failed');
+    throw new Error("load RushConfiguration from rush-sdk failed");
   }
 
   let rushConfiguration = RushLib.RushConfiguration.loadFromDefaultLocation({
--- rush-plugins/rush-sortpack/src/index.ts
+++ rush-plugins/rush-sortpack/src/index.ts
@@ -2,11 +2,32 @@
 
 import { resolve } from "path";
 import RushLib from "@microsoft/rush-lib";
+import {CommandLineParser} from "@rushstack/ts-command-line";
 import { sortpack } from "./sortpack.js";
 import { getRushConfig } from "./helpers/getRushConfig.js";
 
+
+export class WidgetCommandLine extends CommandLineParser {
+  public constructor() {
+    super({
+      toolFilename: 'widget',
+      toolDescription: 'The "widget" tool is a code sample for using the @rushstack/ts-command-line library.'
+    });
+
+    console.log(
+      this)
+  }
+
+
+}
+
+
 async function bootstrap(): Promise<void> {
   try {
+    const commandLine = new WidgetCommandLine()
+    console.log(process.env)
+    commandLine.execute();
+
     const rushConfig: RushLib.RushConfiguration = getRushConfig();
 
     for (const project of rushConfig.projects) {
