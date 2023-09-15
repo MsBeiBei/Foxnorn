98e5fafc023a60c69b0ac19cfbe792f2f0caa47d
BeiBei <Ms_BeiBei@yeah.net>
2023-09-15 11:17:10 +0800

    sortpack

--- rush-plugins/rush-sortpack/src/index.ts
+++ rush-plugins/rush-sortpack/src/index.ts
@@ -1,44 +1,69 @@
 #!/usr/bin/env node
 
 import { resolve } from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import RushLib from "@microsoft/rush-lib";
-import {CommandLineParser} from "@rushstack/ts-command-line";
+import {
+  ConsoleTerminalProvider,
+  Terminal,
+  Colors,
+} from "@rushstack/node-core-library";
 import { sortpack } from "./sortpack.js";
 import { getRushConfig } from "./helpers/getRushConfig.js";
 
+const terminal: Terminal = new Terminal(new ConsoleTerminalProvider());
 
-export class WidgetCommandLine extends CommandLineParser {
-  public constructor() {
-    super({
-      toolFilename: 'widget',
-      toolDescription: 'The "widget" tool is a code sample for using the @rushstack/ts-command-line library.'
-    });
-
-    console.log(
-      this)
-  }
-
-
+interface Argv {
+  filter?: string;
 }
 
+const argv = yargs(hideBin(process.argv)).argv as Argv;
 
 async function bootstrap(): Promise<void> {
   try {
-    const commandLine = new WidgetCommandLine()
-    console.log(process.env)
-    commandLine.execute();
-
     const rushConfig: RushLib.RushConfiguration = getRushConfig();
 
-    for (const project of rushConfig.projects) {
-      const packageJsonFilePath: string = resolve(
-        rushConfig.rushJsonFolder,
-        project.projectFolder
-      );
+    const getPackageJsonFilePath = (projectFolder: string): string =>
+      resolve(rushConfig.rushJsonFolder, projectFolder);
+
+    if (argv?.filter) {
+      const project = rushConfig.projects.find(
+        (project) => project.packageName === argv.filter
+      ) as unknown as RushLib.RushConfigurationProject;
+
+      if (!project) {
+        throw new Error(
+          `The project name "${argv.filter}" passed to "--filter" does not exist in rush.json.`
+        );
+      }
+
+      const packageJsonFilePath = getPackageJsonFilePath(project.projectFolder);
 
       sortpack(packageJsonFilePath);
+
+      terminal.writeLine(
+        Colors.green(`Sort ${argv.filter}'s package.json successfully`)
+      );
+      return;
     }
-  } catch (e) {}
+
+    for (const project of rushConfig.projects) {
+      const packageJsonFilePath = getPackageJsonFilePath(project.projectFolder);
+
+      sortpack(packageJsonFilePath);
+
+      terminal.writeLine(Colors.green(`Sort all package.json successfully`));
+    }
+  } catch (e: any) {
+    if (e.message) {
+      terminal.writeErrorLine(e.message);
+    } else {
+      throw e;
+    }
+
+    process.exit(1);
+  }
 }
 
 bootstrap();
