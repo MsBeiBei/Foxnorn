5be3aac4b0a3d0cca458630b98c9842147cd3274
BeiBei <Ms_BeiBei@yeah.net>
2023-10-17 21:28:11 +0800

    初始化

--- heft-plugins/heft-typescript-plugin/src/TypeScriptCore.ts
+++ heft-plugins/heft-typescript-plugin/src/TypeScriptCore.ts
@@ -1,18 +1,18 @@
 import { dirname } from "path";
 import type TTypeScript from "typescript";
 
-export interface Performance {
-  mark(name: string): void;
-  measure(name: string, startMark?: string, endMark?: string): void;
-  getDuration(measureName: string): number;
-  getCount(measureName: string): number;
+export interface IExtendedTypeScript {
+  performance: {
+    mark(name: string): void;
+    measure(name: string, startMark?: string, endMark?: string): void;
+    getDuration(measureName: string): number;
+    getCount(measureName: string): number;
+  };
 }
 
-export type ExtendedTypeScript = typeof TTypeScript & {
-  performance: Performance;
-};
+export type ExtendedTypeScript = typeof TTypeScript & IExtendedTypeScript;
 
-export interface ProgramOptions<T extends TTypeScript.BuilderProgram> {
+export interface IProgramOptions<T extends TTypeScript.BuilderProgram> {
   rootNames: readonly string[];
   options: TTypeScript.CompilerOptions;
   host?: TTypeScript.CompilerHost;
@@ -21,7 +21,15 @@ export interface ProgramOptions<T extends TTypeScript.BuilderProgram> {
   createProgram?: TTypeScript.CreateProgram<T>;
 }
 
+export interface ICachedEmitModuleKind {
+  moduleKind: TTypeScript.ModuleKind;
+  outFolderPath: string;
+  extension?: string;
+}
+
 const INNER_EMIT_SYMBOL: unique symbol = Symbol("emit");
+const INNER_GET_COMPILER_OPTIONS_SYMBOL: unique symbol =
+  Symbol("getCompilerOptions");
 
 export class TypeScriptCore {
   private readonly tsconfig: TTypeScript.ParsedCommandLine;
@@ -38,7 +46,7 @@ export class TypeScriptCore {
       this.tsconfig.options
     );
 
-    const p: any = this.createProgram({
+    this.createProgram({
       rootNames: this.tsconfig.fileNames,
       options: this.tsconfig.options,
       projectReferences: this.tsconfig.projectReferences,
@@ -47,18 +55,9 @@ export class TypeScriptCore {
         this.tsconfig
       ),
     });
-
-    this.ts.createProgram(
-      this.tsconfig.fileNames,
-      this.tsconfig.options,
-      host,
-      un,
-      configFileParsingDiagnostics,
-      projectReferences
-    )
-
-    p.emit(undefined, this.ts.sys.writeFile, undefined, true, undefined);
   }
+
+  public complieWatch() {}
 
   private createProgram<
     T extends TTypeScript.BuilderProgram = TTypeScript.EmitAndSemanticDiagnosticsBuilderProgram
@@ -69,7 +68,7 @@ export class TypeScriptCore {
     projectReferences,
     host,
     createProgram,
-  }: ProgramOptions<T>) {
+  }: IProgramOptions<T>) {
     host = host || this.ts.createIncrementalCompilerHost(options);
 
     createProgram =
@@ -81,6 +80,7 @@ export class TypeScriptCore {
 
     const program: T & {
       [INNER_EMIT_SYMBOL]?: TTypeScript.Program["emit"];
+      [INNER_GET_COMPILER_OPTIONS_SYMBOL]?: TTypeScript.Program["getCompilerOptions"];
     } = createProgram(
       rootNames,
       options,
@@ -90,13 +90,16 @@ export class TypeScriptCore {
       projectReferences
     );
 
+    program[INNER_EMIT_SYMBOL] = program.emit;
+    program[INNER_GET_COMPILER_OPTIONS_SYMBOL] = program.getCompilerOptions;
+
     program.emit = (
       targetSourceFile?: TTypeScript.SourceFile,
       writeFile?: TTypeScript.WriteFileCallback,
       cancellationToken?: TTypeScript.CancellationToken,
       emitOnlyDtsFiles?: boolean,
       customTransformers?: TTypeScript.CustomTransformers
-    ): TTypeScript.EmitResult | any => {
+    ): TTypeScript.EmitResult => {
       if (emitOnlyDtsFiles) {
         return program[INNER_EMIT_SYMBOL]!(
           targetSourceFile,
@@ -106,7 +109,17 @@ export class TypeScriptCore {
           customTransformers
         );
       }
+
+      const diagnostics: TTypeScript.Diagnostic[] = [];
+      let emitSkipped: boolean = false;
+
+      return {
+        emitSkipped,
+        diagnostics,
+      };
     };
+
+    return program;
   }
 
   private loadTsconfigFile(): TTypeScript.ParsedCommandLine {
@@ -130,4 +143,35 @@ export class TypeScriptCore {
 
     return config;
   }
+}
+
+export type ModuleFormat = ".cjs" | ".mjs" | ".js";
+
+const JS_EXTENSION_REGEX: RegExp = /\.js(\.map)?$/;
+
+export function writeFile(
+  callback: TTypeScript.WriteFileCallback,
+  format?: ModuleFormat
+) {
+  if (!format) {
+    return callback;
+  }
+  const replacementExtension: string = `${format}$1`;
+
+  return (
+    fileName: string,
+    text: string,
+    writeByteOrderMark: boolean,
+    onError?: (message: string) => void,
+    sourceFiles?: readonly TTypeScript.SourceFile[],
+    data?: TTypeScript.WriteFileCallbackData
+  ) =>
+    callback(
+      fileName.replace(JS_EXTENSION_REGEX, replacementExtension),
+      text,
+      writeByteOrderMark,
+      onError,
+      sourceFiles,
+      data
+    );
 }
