91fe3e4e0851d50c555d8c849e596567cd31e0e3
BeiBei <Ms_BeiBei@yeah.net>
2023-10-18 11:57:48 +0800

    初始化

--- heft-plugins/heft-typescript-plugin/src/TypeScriptPlugin.ts
+++ heft-plugins/heft-typescript-plugin/src/TypeScriptPlugin.ts
@@ -1,65 +1,9 @@
 import type { IHeftTaskSession, HeftConfiguration } from "@rushstack/heft";
-import { type ITerminal } from "@rushstack/node-core-library";
-import {
-  HeftTypeScriptPlugin,
-  type ITypeScriptConfigurationJson,
-  type ITsconfigJson,
-} from "./HeftTypeScriptPlugin";
-import {
-  TypeScriptBuilder,
-  type ITypeScriptBuilderConfiguration,
-} from "./TypeScriptBuilder";
+import { HeftTypeScriptPlugin } from "./HeftTypeScriptPlugin";
 
 export default class TypeScriptPlugin extends HeftTypeScriptPlugin {
   override async run(
     taskSession: IHeftTaskSession,
     heftConfiguration: HeftConfiguration
-  ) {
-    await this.getTypeScriptBuilderAsync(taskSession, heftConfiguration);
-  }
-
-  private async getTypeScriptBuilderAsync(
-    taskSession: IHeftTaskSession,
-    heftConfiguration: HeftConfiguration
-  ): Promise<TypeScriptBuilder | undefined> {
-    const typeScriptConfigurationJson:
-      | ITypeScriptConfigurationJson
-      | undefined = await this.loadTypeScriptConfigurationFileAsync(
-      taskSession,
-      heftConfiguration
-    );
-
-    const tsconfigJson: ITsconfigJson | undefined =
-      await this.loadTsconfigFileAsync(
-        taskSession,
-        heftConfiguration,
-        typeScriptConfigurationJson
-      );
-
-    if (!tsconfigJson) {
-      return Promise.resolve(undefined);
-    }
-
-    const terminal: ITerminal = taskSession.logger.terminal;
-
-    const typeScriptToolPath: string =
-      await heftConfiguration.rigPackageResolver.resolvePackageAsync(
-        "typescript",
-        terminal
-      );
-
-    const tsconfigFilePath = this.getTsconfigFilePath(
-      heftConfiguration,
-      typeScriptConfigurationJson
-    );
-
-    const typeScriptBuilderConfiguration: ITypeScriptBuilderConfiguration = {
-      buildFolderPath: heftConfiguration.buildFolderPath,
-      typeScriptToolPath,
-      tsconfigFilePath,
-      heftLogger: this.heftLogger,
-    };
-
-    return new TypeScriptBuilder(typeScriptBuilderConfiguration);
-  }
+  ) {}
 }
